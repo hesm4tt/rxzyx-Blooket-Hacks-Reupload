950828c4953569f57879bf3dcf30ed9f737d6ba8
mattleeyy <86572794+hesm4tt@users.noreply.github.com>
2023-02-11 22:48:33 -0500

    Delete Blooket directory
    skidded version of someone else’s scripts

--- Blooket/Default-Scripts/All-Answers-Correct.js
+++
@@ -1,6 +0,0 @@
-setInterval(function () {
-  var hack = Object.values(document.querySelector('#app > div > div'))[1].children[0]['_owner'].stateNode.state.question
-  try {
-    hack.correctAnswers = hack.answers
-  } catch (err) {}
-})
--- Blooket/Default-Scripts/Get-Folder.js
+++
@@ -1,4 +0,0 @@
-var hack = Object.values(document.querySelector('#app > div > div'))[1].children[1]['_owner'].stateNode;
-hack.setState({
-  creatingFolder: true
-});
